841fa22d3154bba7132b4741a9ff4bf6fb230227
Nhan <114342100+LittleHundNhan@users.noreply.github.com>
2025-11-10 02:33:21 +0700

    Peak

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Pathfinding;
+
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    private void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+}
