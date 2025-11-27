5517e44c219a90484ca6ea97923278d9cf5f4978
Leon02468 <123917231+Leon02468@users.noreply.github.com>
2025-11-27 10:20:22 +0700

    Anything idk

---
+++ Assets/Scripts/Mantis/ConfinerSwitcher.cs
@@ -0,0 +1,9 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ConfinerSwitcher : MonoBehaviour
+{
+    public CinemachineConfiner2D confiner;
+    public PolygonCollider2D bossConfiiner;
+    public PolygonCollider2D worldConfiiner;
+}
